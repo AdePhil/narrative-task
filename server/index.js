82e644b0f51caf4b4cf64d3bc20a4f4006443b43
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2022-01-05 20:59:20 +0100

    feat: add env

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+// Initialize app
 const app = express();
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors("*"));
 
 //start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
