3ddeb785ee6da540fa6d7c132761bda66ade46e1
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2022-01-06 19:31:52 +0100

    feat: add loader

---
+++ client/src/components/Loader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4 m-6 loader-container d-flex justify-center align-center">
+    <img src="/spinner.svg" class="loader-img" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.loader-container {
+  min-height: 400px;
+}
+.loader-img {
+  width: 50px;
+}
+</style>
