b879ae4e3f3ffdadc0aafea29d682448c59cefca
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2022-01-06 20:09:37 +0100

    feat: fetch data package endpoint

--- server/controller/dataPackage.js
+++ server/controller/dataPackage.js
@@ -3,6 +3,19 @@ const { DataPackageType } = require("../models");
 const createDataPackage = async (req, res) => {
   try {
     const dataPackage = await DataPackageType.create(req.body);
+    return res.status(201).json({
+      message: "Data package type created successfully",
+      data: dataPackage,
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).json({ message: "Error saving to db" });
+  }
+};
+
+const getDataPackage = async (req, res) => {
+  try {
+    const dataPackage = await DataPackageType.findAll();
     return res.status(200).json({
       message: "Data package type created successfully",
       data: dataPackage,
@@ -15,4 +28,5 @@ const createDataPackage = async (req, res) => {
 
 module.exports = {
   createDataPackage,
+  getDataPackage,
 };
--- server/routes/dataPackage.js
+++ server/routes/dataPackage.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const dataPackageTypeController = require("../controller/dataPackage");
 router.post("/create", dataPackageTypeController.createDataPackage);
+router.get("/all", dataPackageTypeController.getDataPackage);
 
 module.exports = router;
